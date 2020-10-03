3490731ab306d5e0d68ae8b17d47a538a7d0ceb2
sofiekamber <sofie.kamber@gmail.com>
2020-10-03 11:06:59 +0200

    add main scene game

--- src/game.ts
+++ src/game.ts
@@ -1,35 +1,25 @@
 import 'phaser';
+import {Atom} from "./atom";
 
-export default class Demo extends Phaser.Scene {
+export class Game extends Phaser.Scene {
+
+    atom: Atom;
+
     constructor() {
-        super('demo');
+        super('game');
     }
 
     preload() {
-        this.load.image('logo', 'assets/phaser3-logo.png');
-        this.load.image('libs', 'assets/libs.png');
-        this.load.glsl('bundle', 'assets/plasma-bundle.glsl.js');
-        this.load.glsl('stars', 'assets/starfields.glsl.js');
     }
 
     create() {
-        this.add.shader('RGB Shift Field', 0, 0, 800, 600).setOrigin(0);
-
-        this.add.shader('Plasma', 0, 412, 800, 172).setOrigin(0);
-
-        this.add.image(400, 300, 'libs');
-
-        const logo = this.add.image(400, 70, 'logo');
-
-        this.tweens.add({
-            targets: logo,
-            y: 350,
-            duration: 1500,
-            ease: 'Sine.inOut',
-            yoyo: true,
-            repeat: -1
-        });
+        this.atom = new Atom(this);
     }
+
+    update() {
+
+    }
+
 }
 
 const config = {
@@ -37,7 +27,7 @@ const config = {
     backgroundColor: '#125555',
     width: 800,
     height: 600,
-    scene: Demo
+    scene: Game
 };
 
 const game = new Phaser.Game(config);
