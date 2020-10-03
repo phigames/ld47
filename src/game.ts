35ad0ba6b0faeadb07f91cf9d0e51525c4b57de9
Andreas Säuberli <andreas.saeuberli@gmail.com>
2020-10-03 10:25:42 +0200

    important changes

--- src/game.ts
+++ src/game.ts
@@ -1,22 +1,18 @@
 import 'phaser';
 
-export default class Demo extends Phaser.Scene
-{
-    constructor ()
-    {
+export default class Demo extends Phaser.Scene {
+    constructor() {
         super('demo');
     }
 
-    preload ()
-    {
+    preload() {
         this.load.image('logo', 'assets/phaser3-logo.png');
         this.load.image('libs', 'assets/libs.png');
         this.load.glsl('bundle', 'assets/plasma-bundle.glsl.js');
         this.load.glsl('stars', 'assets/starfields.glsl.js');
     }
 
-    create ()
-    {
+    create() {
         this.add.shader('RGB Shift Field', 0, 0, 800, 600).setOrigin(0);
 
         this.add.shader('Plasma', 0, 412, 800, 172).setOrigin(0);
@@ -32,7 +28,7 @@ export default class Demo extends Phaser.Scene
             ease: 'Sine.inOut',
             yoyo: true,
             repeat: -1
-        })
+        });
     }
 }
 
